0dc6c097824f4bfa16cdbcac3903d28f30c9e2b2
Patrick O'Neill <synapse.and.syntax@gmail.com>
2012-08-20 01:51:43 -0400

    major upheaval: increment reactions work now

--- computing.R
+++ computing.R
@@ -5,13 +5,30 @@ slow <- 1
 nil <- 0
 g <- 1
 g.ab <- 2
-x <- 3
-x.ab <- 4
-x.prime <- 5
+g.prime.ab <- 3
+x <- 4
+x.ab <- 5
+x.prime <- 6
+x.rx <- 7
+done <- 8
 identity.reactions <- list(list(c(nil),c(x.ab),slow),
                            list(c(x,x.ab),c(x),fast),
                            list(c(x.ab,x.ab),c(x.ab),fast))
 
-increment.reactions <- list(list(c(g,x),c(x.prime,g),slow),
-                           list(c(g,x.ab),c(nil),slow),
-                           list(c(g.ab,x.ab),c(x.ab),fast))
+increment.reactions <- list(list(c(nil),c(x.ab),slow),
+                            list(c(x,x.ab),c(x),fast),
+                            list(c(x.ab,x.ab),c(x.ab),fast),#maintenance for x
+                            list(c(x,g),c(x.prime,g),slow),#19
+                            list(c(g,x.ab),c(nil),slow),#20
+                            #secondary absence indicator, eqs. 13-38
+                            list(c(nil),c(g.ab),slow),#13
+                            list(c(g,g.ab),c(g),fast),#14
+                            list(c(g.ab,g.ab),c(g.ab),fast),#15
+                            list(c(g.ab),c(g.prime.ab),slow),#16
+                            list(c(g,g.prime.ab),c(g),fast),#17
+                            list(c(g.prime.ab,g.prime.ab),c(g.prime.ab),fast),#18
+                            list(c(g.prime.ab,x.prime,x.prime),c(x,x.prime,x.rx),fast),#21
+                            list(c(x.rx),c(nil),slow),
+                            #list(c(x.rx,x.prime,g.prime.ab),c(nil),slow),#23 (dec)
+                            list(c(x.rx,x.prime,g.prime.ab),c(x,x,done),slow)#24 (inc)
+                            )
--- stochastic_model.R
+++ stochastic_model.R
@@ -3,9 +3,9 @@
 
 
 # A model is a list of reactions and an initial state.  A reaction is
-# a list of (1) a vector of reactants, (2) a vector of products, and
-# (3) a reaction rate constant.  We assume that the effective rate is given by
-# mass-action kinetics, i.e. that for a reaction of the form:
+# a (1) stoichiometric vector and (2) a reaction rate constant.  We
+# assume that the effective rate is given by mass-action kinetics,
+# i.e. that for a reaction of the form:
 
 # v_1A_1 + v_2A_2 + ... + v_1A_n -> ...
 
@@ -30,7 +30,67 @@ RED <- 1
 YELLOW <- 2
 NIL <- 0
 oriole.init.state <- c(0,1)
+reagent.complex <- function(reaction.input)reaction.input[[1]]
+product.complex <- function(reaction.input)reaction.input[[2]]
+# A reaction input is a human-readable representation of a reaction,
+# for example: list(c(RED),c(RED,RED),lambda.r) where the first vector
+# describes the reagents, the second the products, and the third entry
+# the reaction rate.
 
+stoich.from.complex <- function(complex,state){
+  sapply(1:length(state),function(i)sum(complex==i))
+}
+to.stoich <- function(reaction.input,state){
+  reagents <- reagent.complex(reaction.input)
+  products <- product.complex(reaction.input)
+  reagents.vector <- stoich.from.complex(reagents,state)
+  products.vector <- stoich.from.complex(products,state)
+  products.vector - reagents.vector
+}
+
+state.length <- function(hr.model){
+  max(unlist(lapply(hr.model,
+                    function(reaction)max(c(reaction[[1]],reaction[[2]])))))
+}
+reduce.enzymatic.reactions <- function(hr.model){
+  #Given a human readable list of reactions, return a human readable
+  #list with one less enzymatic reaction.  A difficulty lies in the
+  #fact that converting enzymatic reactions will require access to the
+  #length of the state vector in order to assign a new component to
+  #the intermediate complex species.
+  intermediate.rate <- 1000
+  enzymatics <- lapply(hr.model,is.enzymatic.reaction)
+  if(!any(sapply(enzymatics,function(x)x))){
+    return(hr.model)
+  }
+  else{
+    reaction.index <- which(enzymatics==TRUE)[1]
+    reaction <- hr.model[[reaction.index]]
+    reagents <- reagent.complex(reaction)
+    products <- product.complex(reaction)
+    n <- state.length(hr.model)
+    intermediate <- n + 1
+    to.intermediate <- list(reagents,
+                            intermediate,
+                            intermediate.rate)
+    from.intermediate <- list(intermediate,
+                              reagents, 
+                            intermediate.rate)
+    to.products <- list(intermediate,products,rate(reaction))
+    new.reactions <- list(to.intermediate,from.intermediate,to.products)
+    reduce.enzymatic.reactions(c(hr.model[-reaction.index],new.reactions))
+  }
+}
+explicit.model <- function(hr.model){
+  #Given a model description in human readable format
+  #(e.g. oriole.reactions), expand full stochiometric vectors
+  state <- 1:state.length(hr.model)
+  lapply(hr.model,function(reaction)(list(to.stoich(reaction,state),
+                                               reaction[[3]])))
+}
+is.enzymatic.reaction <- function(hr.reaction){
+  any(reagent.complex(hr.reaction) %in% product.complex(hr.reaction))
+}
 model.stochastic <- function(lambda.r,lambda.y,mu.r,mu.y,tau.yr,tau.ry,init.state){
   
   oriole.reactions <- list(list(c(RED),c(RED,RED),lambda.r),
@@ -44,26 +104,33 @@ model.stochastic <- function(lambda.r,lambda.y,mu.r,mu.y,tau.yr,tau.ry,init.stat
 }
 see <- function(x){print(x)
                    x}
-reagent.coeffs <- function(reaction)reaction[[1]]
-product.coeffs <- function(reaction)reaction[[2]]
 
 reagent.abundances <- function(reaction,state){
-  state[reagent.coeffs(reaction)]
+  state[reagent.complex(reaction)]
 }
 
 rate <- function(reaction)reaction[[3]]
 
+stoich.coeffs <- function(complex,state){
+  sapply(1:length(state),function(i){how.many(complex==i)})
+}
+
+stoich.from <- function(reaction){
+  reaction[[1]]
+}
+rate.from <- function(reaction){
+  reaction[[2]]
+}
+
+effective.rate <- function(reaction,state){
+    stoich.vec <- stoich.from(reaction)
+    rate <- rate.from(reaction)
+    stoichs <- -sapply(stoich.vec,function(x)min(x,0))
+    rate * prod(factorial(stoichs) * choose(state,stoichs))
+    }
+
 effective.rates <- function(reactions,state){
-  effective.rate <- function(reaction){
-    if(all(reagent.coeffs(reaction)==0)){#if reaction is a pure birth process...
-      rate(reaction) #return the rate
-    }
-    else{#return the stoichiometric product
-      (prod(reagent.abundances(reaction,state))
-       * rate(reaction))
-    }
-  }
-    unlist(lapply(reactions,effective.rate))
+    unlist(lapply(reactions,function(reaction)effective.rate(reaction,state)))
 }
 
 safe.rexp <- function(rate){
@@ -81,12 +148,12 @@ sample.reaction <- function(reactions,state){
 update.state <- function(reactions,state,reaction.index){
   #Given a reaction model, a state and the next index (first argument
   #of return from sample.reaction, return an updated state vector)
-  reaction <- reactions[[reaction.index]]
-  reagents <- reagent.coeffs(reaction)
-  products <- product.coeffs(reaction)
-  delta.reagents <- sapply(1:length(state),function(i)how.many(reagents == i))
-  delta.products <- sapply(1:length(state),function(i)how.many(products == i))
-  state - delta.reagents + delta.products
+  reaction <- stoich.from(reactions[[reaction.index]])
+  ## reagents <- reagent.complex(reaction)
+  ## products <- product.complex(reaction)
+  ## delta.reagents <- sapply(1:length(state),function(i)how.many(reagents == i))
+  ## delta.products <- sapply(1:length(state),function(i)how.many(products == i))
+  state + reaction
 }
 
 simulate <- function(reactions,state,max.time,trajectory=TRUE){
@@ -99,6 +166,8 @@ simulate <- function(reactions,state,max.time,trajectory=TRUE){
     reaction.index <- index.and.time[1]
     reaction.time  <- index.and.time[2]
     iter <- iter + c(1,reaction.time)
+    #print(iter[2])
+    print(floor(state))
     if(reaction.time == Inf || iter[2] > max.time){
       break
     }
@@ -146,4 +215,10 @@ results.stochastic <- function(lambda.r,#speciation R
     }
   }
   results
+}
+
+display.hr.model <- function(hr.model){
+  for(reaction in hr.model){
+    cat((reagent.complex(reaction)),"->",product.complex(reaction),"rate:",rate(reaction),"\n")
+  }
 }
